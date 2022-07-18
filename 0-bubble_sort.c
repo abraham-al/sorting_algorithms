6693366e99a5530b852c6017fbedee39a3b2ad42
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-07-18 17:35:31 +0300

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,28 @@
 #include "sort.h"
+
 /**
- * bubble_sort - sorts an array of integers in asc order
- *
- * @array: an array store
- * @size: sizesssss
+ * bubble_sort - sort list with bubble
+ * @array: The array to be printed
+ * @size: Number of elements in @array
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, j;
-int tmp;
-if (size < 2)
-return;
-for (i = 0; i < size; i++)
-{
-for (j = 0; j < size - 1 - i; j++)
-{
-if (array[j] > array[j + 1])
-{
-tmp = array[j];
-array[j] = array[j + 1];
-array[j + 1] = tmp;
-print_array(array, size);
-}
-}
-}
+	size_t n = 0, t = size;
+	int holder;
+
+	if (array == NULL)
+		return;
+	for (t = size; t > 0; t--)
+	{
+		for (n = 0; n < size - 1; n++)
+		{
+			if (array[n] > array[n + 1])
+			{
+				holder = array[n];
+				array[n] = array[n + 1];
+				array[n + 1] = holder;
+				print_array(array, size);
+			}
+		}
+	}
 }
